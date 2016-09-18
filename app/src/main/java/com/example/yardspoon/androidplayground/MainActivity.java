296730d9fb2c833b68b65255681c7b5aecd33b15
Benjamin P Lee <benjaminplee@gmail.com>
2016-09-17 22:10:58 -0500

    Working on layout

--- app/src/main/java/com/example/yardspoon/androidplayground/MainActivity.java
+++ app/src/main/java/com/example/yardspoon/androidplayground/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.yardspoon.androidplayground;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner stateSpinner = (Spinner) findViewById(R.id.state);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.states, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stateSpinner.setAdapter(adapter);
     }
 }
